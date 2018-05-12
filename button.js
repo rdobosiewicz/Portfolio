763aaf70dfa01bb7a71d6df7121336655cc5ad37
rdobosiewicz <dobosiewicz.rafal@gmail.com>
2018-05-12 13:36:23 +0200

    Dodanie button i zaktualizowanie strony

---
+++ button.js
@@ -0,0 +1,7 @@
+var butt1 = document.querySelector("#button1");
+
+butt1.addEventListener("click", clickButton, false);
+
+function clickButton(){
+	window.open("https://github.com/rdobosiewicz");
+}
